a99c4dd7880df73843ff5cc4b72934dc85f6e6ca
NicoSelomin <vodounnounicodemeselomin@gmail.com>
2024-09-17 11:37:25 +0100

    code updated

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.use((req, res) =>{
-    res.json({"message":"Votre requête est bien reçu"})
-})
+//middlewares
+app.use((req, res, next) => {
+  console.log("Requête reçue !");
+  next();
+});
 
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.json({ message: "Votre requête a bien été reçue !" });
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("Réponse envoyée avec succès !");
+});
 
 module.exports = app;
